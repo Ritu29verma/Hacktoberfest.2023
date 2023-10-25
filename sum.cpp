dbcea093d7203bfac8d4ebd8ba3ee3183050ed0a
Ritu Verma <ritu1298verma@gmail.com>
2023-10-25 16:14:18 +0530

    sum.cpp
    In this program we have given a number N. We are finding the sum of all the digits of N

---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int sumOfDigits(int N){
+        //code here
+        if(N<10) return N;
+        return ((N%10) + sumOfDigits(N/10));
+    }
+};
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0; 
+}  
